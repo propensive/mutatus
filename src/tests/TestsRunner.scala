b61d7f43837280a31f5db02dcf4a90bde0098cf6
wojciechmazur <wojciech.mazur95@gmail.com>
2020-04-05 22:37:26 +0200

    Restored QueryBuilder spec

--- src/tests/TestsRunner.scala
+++ src/tests/TestsRunner.scala
@@ -6,16 +6,21 @@ import util.Try
 import scala.util.Failure
 import scala.util.Success
 
+import com.google.cloud.NoCredentials
+import com.google.cloud.datastore.StructuredQuery._
+import com.google.cloud.datastore._
+
 object TestsRunner extends TestApp {
   override def tests(): Unit = {
     MutatusSerializationSpec()
-    // MutatusQueryBuilderSpec() //TODO restore after #7 query builder merge
+    MutatusQueryBuilderSpec()
 
     Try(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
       .map { _ =>
         if (Try(System.getenv("MUTATUS_TESTS_E2E"))
               .map(_.toUpperCase() == "TRUE")
               .getOrElse(false)) {
+
           println("Running E2E tests")
           val _ = MutatusE2ESpec()
         } else {
